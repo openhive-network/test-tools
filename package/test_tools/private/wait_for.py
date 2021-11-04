21f356b681b73d69df86d0e917db6e19f08ab0c4
Piotr Batko <pbatko@syncad.com>
2021-11-04 16:29:00 +0100

    Add helper function wait_for_event

--- package/test_tools/private/wait_for.py
+++ package/test_tools/private/wait_for.py
@@ -1,5 +1,11 @@
 import math
 import time
+from typing import Optional, TYPE_CHECKING
+
+
+if TYPE_CHECKING:
+    from threading import Event
+
 
 def wait_for(predicate, *, timeout=math.inf, timeout_error_message=None, poll_time=1.0):
     assert timeout >= 0
@@ -14,3 +20,23 @@ def wait_for(predicate, *, timeout=math.inf, timeout_error_message=None, poll_ti
         already_waited += sleep_time
 
     return already_waited
+
+
+def wait_for_event(event: 'Event',
+                   deadline: Optional[float] = None,
+                   exception: Optional[Exception] = None) -> None:
+    """
+    Blocks current thread execution until `event` is set. Optionally raises `exception`, when
+    `deadline` is reached.
+
+    :param event: Awaited event. When event is set functions stops blocking.
+    :param deadline: Time point before which event must occur. Can be counted from the formula:
+                     deadline = now + timeout
+    :param exception: Raised when deadline is reached. If None, raises default TimeoutError.
+    """
+    if exception is None:
+        exception = TimeoutError('The event didn\'t occur within given time frame')
+
+    timeout = deadline - time.time()
+    if not event.wait(timeout):
+        raise exception
