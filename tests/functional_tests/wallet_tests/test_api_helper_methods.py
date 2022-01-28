3667904b3e54d5ef2b3a4e6137f1bb1222456d54
Piotr Batko <pbatko@syncad.com>
2022-01-28 10:59:16 +0100

    Add helper functions for multiple account creation and listing

---
+++ tests/functional_tests/wallet_tests/test_api_helper_methods.py
@@ -0,0 +1,26 @@
+import pytest
+
+from test_tools import Wallet
+
+
+@pytest.fixture
+def wallet(world, request):
+    init_node = world.create_init_node()
+
+    shared_file_size = request.node.get_closest_marker('node_shared_file_size')
+    if shared_file_size:
+        init_node.config.shared_file_size = shared_file_size.args[0]
+
+    init_node.run()
+
+    return Wallet(attach_to=init_node)
+
+
+@pytest.mark.node_shared_file_size('16G')
+def test_creation_of_huge_number_of_accounts(wallet):
+    accounts_before = set(wallet.list_accounts())
+    created_accounts = wallet.create_accounts(100_000)
+    accounts_after = set(wallet.list_accounts())
+
+    assert len(created_accounts) == 100_000
+    assert len(accounts_after.difference(accounts_before)) == 100_000
