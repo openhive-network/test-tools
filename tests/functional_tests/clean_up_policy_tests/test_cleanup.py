a0b852cff3e776fc6a76dfb153aa121e0def0cc7
Piotr Batko <pbatko@syncad.com>
2022-03-04 13:55:17 +0100

    Refactor node cleanup tests -- merge them with existing cleanup tests

--- tests/functional_tests/clean_up_policy_tests/test_cleanup.py
+++ tests/functional_tests/clean_up_policy_tests/test_cleanup.py
@@ -1,15 +1,57 @@
 from typing import Optional
 
+import pytest
+
 from test_tools import clean_up_policy
-from test_tools.constants import CleanUpPolicy
-from test_tools.private.utilities_for_tests.node_files import important_files_are_removed, unneeded_files_are_removed
+from test_tools.constants import CleanUpPolicy, NodeCleanUpPolicy
 
 
-def check_if_files_are_removed(world,
-                               policy: Optional[CleanUpPolicy] = None,
-                               *,
-                               remove_important_files: bool,
-                               remove_unneeded_files: bool):
+def important_files_are_removed(node):
+    paths_of_important_files = [
+        '.',
+        'config.ini',
+        'stderr.txt',
+    ]
+
+    return all(not node.directory.joinpath(path).exists() for path in paths_of_important_files)
+
+
+def unneeded_files_are_removed(node):
+    paths_of_unneeded_files = [
+        'blockchain/block_log',
+    ]
+
+    return all(not node.directory.joinpath(path).exists() for path in paths_of_unneeded_files)
+
+
+def check_if_node_files_are_removed(world,
+                                    policy: Optional[CleanUpPolicy] = None,
+                                    *,
+                                    remove_important_files: bool,
+                                    remove_unneeded_files: bool):
+    init_node = world.create_init_node()
+    init_node.run()
+
+    if policy is not None:
+        corresponding_clean_up_policy = {
+            CleanUpPolicy.REMOVE_EVERYTHING: NodeCleanUpPolicy.REMOVE_EVERYTHING,
+            CleanUpPolicy.REMOVE_ONLY_UNNEEDED_FILES: NodeCleanUpPolicy.REMOVE_ONLY_UNNEEDED_FILES,
+            CleanUpPolicy.DO_NOT_REMOVE_FILES: NodeCleanUpPolicy.DO_NOT_REMOVE_FILES,
+        }
+
+        init_node.set_clean_up_policy(corresponding_clean_up_policy[policy])
+
+    world.close()
+
+    assert important_files_are_removed(init_node) == remove_important_files
+    assert unneeded_files_are_removed(init_node) == remove_unneeded_files
+
+
+def check_if_everyone_files_are_removed(world,
+                                        policy: Optional[CleanUpPolicy] = None,
+                                        *,
+                                        remove_important_files: bool,
+                                        remove_unneeded_files: bool):
     # Create some nodes outside of a network
     init_node = world.create_init_node()
     api_node = world.create_api_node()
@@ -36,7 +78,17 @@ def check_if_files_are_removed(world,
         assert all(unneeded_files_are_removed(node) == remove_unneeded_files for node in network.nodes())
 
 
-def test_default_policy(world):
+run_for_all_cases = pytest.mark.parametrize(
+    'check_if_files_are_removed',
+    [
+        check_if_node_files_are_removed,
+        check_if_everyone_files_are_removed,
+    ]
+)
+
+
+@run_for_all_cases
+def test_default_policy(world, check_if_files_are_removed):
     check_if_files_are_removed(
         world,
         remove_important_files=False,
@@ -44,7 +96,8 @@ def test_default_policy(world):
     )
 
 
-def test_do_not_remove_files_clean_up_policy(world):
+@run_for_all_cases
+def test_do_not_remove_files_clean_up_policy(world, check_if_files_are_removed):
     check_if_files_are_removed(
         world,
         CleanUpPolicy.DO_NOT_REMOVE_FILES,
@@ -53,7 +106,8 @@ def test_do_not_remove_files_clean_up_policy(world):
     )
 
 
-def test_remove_only_unneeded_files_clean_up_policy(world):
+@run_for_all_cases
+def test_remove_only_unneeded_files_clean_up_policy(world, check_if_files_are_removed):
     check_if_files_are_removed(
         world,
         CleanUpPolicy.REMOVE_ONLY_UNNEEDED_FILES,
@@ -62,7 +116,8 @@ def test_remove_only_unneeded_files_clean_up_policy(world):
     )
 
 
-def test_remove_everything_clean_up_policy(world):
+@run_for_all_cases
+def test_remove_everything_clean_up_policy(world, check_if_files_are_removed):
     check_if_files_are_removed(
         world,
         CleanUpPolicy.REMOVE_EVERYTHING,
