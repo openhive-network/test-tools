0fb0ec7c8b4e3e5518816388750fc231513337c5
Piotr Batko <pbatko@syncad.com>
2022-03-25 15:35:53 +0100

    Accept None as argument in `get_handle` and `get_implementation`
    It simplifies writing glue code on handle-implementation border. When some
    implementation object returns optionally another implementation object,
    glue-code will have to checks if object is None and then skip conversion.
    Now it will work without any additional checks on user site.

--- package/test_tools/private/user_handles/get_implementation.py
+++ package/test_tools/private/user_handles/get_implementation.py
@@ -1,13 +1,21 @@
+from typing import Any, Optional
+
 from test_tools.private.user_handles.implementation import Implementation
 from test_tools.private.user_handles.handles.remote_node_handle import RemoteNodeHandle
 
 
-def get_implementation(handle):
+def get_implementation(handle: Optional[Any]) -> Optional[Implementation]:
+    if handle is None:
+        return None
+
     if isinstance(handle, RemoteNodeHandle):
         return handle._RemoteNodeHandle__implementation  # pylint: disable=protected-access
 
     raise RuntimeError(f'Unable to get implementation for {handle}')
 
 
-def get_handle(implementation: Implementation):
+def get_handle(implementation: Optional[Implementation]) -> Optional[Any]:
+    if implementation is None:
+        return None
+
     return implementation._Implementation__handle  # pylint: disable=protected-access
