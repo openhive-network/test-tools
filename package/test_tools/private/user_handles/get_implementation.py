99a3a23d6702f3134b0c6c05897b97910c6dabf4
Piotr Batko <pbatko@syncad.com>
2022-03-31 14:35:13 +0200

    Simplify get_implementation helper function
    Now all handles inherits from Handle class, so each of them have
    `_Handle__implementation` member. There is no need to write switch-like
    proper implementation selector anymore.

--- package/test_tools/private/user_handles/get_implementation.py
+++ package/test_tools/private/user_handles/get_implementation.py
@@ -1,28 +1,16 @@
-from typing import Any, Optional
+from __future__ import annotations
 
-from test_tools.private.user_handles.implementation import Implementation
+from typing import Any, Optional, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from test_tools.private.user_handles.implementation import Implementation
 
 
 def get_implementation(handle: Optional[Any]) -> Optional[Implementation]:
-    # pylint: disable=import-outside-toplevel
-    # Break import-cycle
-    from test_tools.private.user_handles.handles.node_handles.remote_node_handle import RemoteNodeHandle
-    from test_tools.private.user_handles.handles.node_handles.node_handle_base import NodeHandleBase
-    from test_tools.private.user_handles.handles.network_handle import NetworkHandle  # pylint: disable=cyclic-import
-
     if handle is None:
         return None
 
-    if isinstance(handle, RemoteNodeHandle):
-        return handle._RemoteNodeHandle__implementation  # pylint: disable=protected-access
-
-    if isinstance(handle, NodeHandleBase):
-        return handle._NodeHandleBase__implementation  # pylint: disable=protected-access
-
-    if isinstance(handle, NetworkHandle):
-        return handle._NetworkHandle__implementation  # pylint: disable=protected-access
-
-    raise RuntimeError(f'Unable to get implementation for {handle}')
+    return handle._Handle__implementation  # pylint: disable=protected-access
 
 
 def get_handle(implementation: Optional[Implementation]) -> Optional[Any]:
