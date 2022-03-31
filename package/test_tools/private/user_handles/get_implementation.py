541f42b987b5a6ffb586d9f3bcdaa8f67129edc6
Piotr Batko <pbatko@syncad.com>
2022-03-31 14:42:43 +0200

    Update Handle's type annotations

--- package/test_tools/private/user_handles/get_implementation.py
+++ package/test_tools/private/user_handles/get_implementation.py
@@ -1,19 +1,20 @@
 from __future__ import annotations
 
-from typing import Any, Optional, TYPE_CHECKING
+from typing import Optional, TYPE_CHECKING
 
 if TYPE_CHECKING:
+    from test_tools.private.user_handles.handle import Handle
     from test_tools.private.user_handles.implementation import Implementation
 
 
-def get_implementation(handle: Optional[Any]) -> Optional[Implementation]:
+def get_implementation(handle: Optional[Handle]) -> Optional[Implementation]:
     if handle is None:
         return None
 
     return handle._Handle__implementation  # pylint: disable=protected-access
 
 
-def get_handle(implementation: Optional[Implementation]) -> Optional[Any]:
+def get_handle(implementation: Optional[Implementation]) -> Optional[Handle]:
     if implementation is None:
         return None
 
--- package/test_tools/private/user_handles/implementation.py
+++ package/test_tools/private/user_handles/implementation.py
@@ -1,7 +1,15 @@
+from __future__ import annotations
+
+from typing import Optional, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from test_tools.private.user_handles.handle import Handle
+
+
 class Implementation:
     """Base class for all objects pointed by handles. Contains handle by which is pointed."""
 
-    def __init__(self, *args, handle, **kwargs):
+    def __init__(self, *args, handle: Optional[Handle], **kwargs):
         # Multiple inheritance friendly, passes arguments to next object in MRO.
         super().__init__(*args, **kwargs)
 
