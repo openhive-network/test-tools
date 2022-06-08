f32538f66ececab6590cd2e225b732d30af3464e
Piotr Batko <pbatko@syncad.com>
2022-06-08 13:48:01 +0200

    Rewrite type annotations with typing.overload

--- package/test_tools/__private/user_handles/get_implementation.py
+++ package/test_tools/__private/user_handles/get_implementation.py
@@ -1,20 +1,40 @@
 from __future__ import annotations
 
-from typing import Optional, TYPE_CHECKING
+from typing import overload, TYPE_CHECKING
 
 if TYPE_CHECKING:
     from test_tools.__private.user_handles.handle import Handle
     from test_tools.__private.user_handles.implementation import Implementation
 
 
-def get_implementation(handle: Optional[Handle]) -> Optional[Implementation]:
+@overload
+def get_implementation(handle: None) -> None:
+    ...
+
+
+@overload
+def get_implementation(handle: Handle) -> Implementation:
+    ...
+
+
+def get_implementation(handle):
     if handle is None:
         return None
 
     return handle._Handle__implementation  # pylint: disable=protected-access
 
 
-def get_handle(implementation: Optional[Implementation]) -> Optional[Handle]:
+@overload
+def get_handle(implementation: None) -> None:
+    ...
+
+
+@overload
+def get_handle(implementation: Implementation) -> Handle:
+    ...
+
+
+def get_handle(implementation):
     if implementation is None:
         return None
 
