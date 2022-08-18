c8f5312294b787376e204cc9a7a020a3e6d139a6
Piotr Batko <pbatko@syncad.com>
2022-08-18 12:42:41 +0200

    Use PrivateKey and PublicKey in Account to generate keys in lazy way
    There is use case, where immediate key generation cause problems, because in
    this case, key generator executable is not provided. Module with tests is run
    in two environments (testnet and mainnet) and in mainnet case, key generator
    is not provided. Account object is created in global variable and is used only
    in testnet case, but global variables are evaluated also in mainnet case, what
    caused problem.
    
    To solve this problem, account object should be generated independently from
    presence of key generator executable. This commit moves key generation to
    moment, when keys are really needed.
    
    Implementations details:
    `with_keys` initializer parameter is not needed anymore, because it's purpose
    was to prevent keys generation in initializer. Now, when keys aren't generated
    there anyway, parameter was removed.

--- package/test_tools/__private/account.py
+++ package/test_tools/__private/account.py
@@ -1,17 +1,17 @@
+from __future__ import annotations
+
+from typing import List
+
 from test_tools.__private.key_generator import KeyGenerator
+from test_tools.__private.keys import PrivateKey, PublicKey
 
 
 class Account:
-    def __init__(self, name, secret='secret', with_keys=True):
+    def __init__(self, name, secret='secret'):
         self.__name = name
         self.__secret = secret
-        self.private_key = None
-        self.public_key = None
-
-        if with_keys:
-            output = KeyGenerator.generate_keys(name, secret=secret)[0]
-            self.private_key = output['private_key']
-            self.public_key = output['public_key']
+        self.__private_key = PrivateKey(self.__name, secret=self.__secret)
+        self.__public_key = PublicKey(self.__name, secret=self.__secret)
 
     @property
     def name(self) -> str:
@@ -21,13 +21,26 @@ class Account:
     def secret(self) -> str:
         return self.__secret
 
+    @property
+    def private_key(self) -> PrivateKey:
+        return self.__private_key
+
+    @property
+    def public_key(self) -> PublicKey:
+        return self.__public_key
+
     @staticmethod
-    def create_multiple(number_of_accounts, name_base='account', *, secret='secret'):
+    def create_multiple(number_of_accounts: int,
+                        name_base: str = 'account',
+                        *,
+                        secret: str = 'secret') -> List[Account]:
         accounts = []
         for generated in KeyGenerator.generate_keys(name_base, number_of_accounts=number_of_accounts, secret=secret):
-            account = Account(generated['account_name'], secret=secret, with_keys=False)
-            account.private_key = generated['private_key']
-            account.public_key = generated['public_key']
+            account = Account(generated['account_name'], secret=secret)
+            # pylint: disable=unused-private-member, protected-access
+            account.__private_key._value = generated['private_key']
+            account.__public_key._value = generated['public_key']
+            # pylint: enable=unused-private-member, protected-access
 
             accounts.append(account)
 
---
+++ tests/unit_tests/account_tests/test_account_lazy_creation.py
@@ -0,0 +1,33 @@
+import pytest
+
+import test_tools as tt
+
+# pylint: disable=unused-import
+# Imported fixture is automatically used, so it's false positive.
+from ..key_generation_tests.local_tools import ensure_that_key_generator_executable_is_missing
+# pylint: enable=unused-import
+
+
+def test_if_account_object_can_be_created_without_key_generator():
+    tt.Account('example')
+
+    # Object is created, but public_key and private_key are not generated
+    # yet, so missing key generator executable is not a problem.
+
+
+def test_if_keys_can_be_accessed_without_generator():
+    account = tt.Account('example')
+    _ = account.public_key, account.private_key
+
+    # Generation of public_key and private_key is postponed even more, until
+    # value will be really required (e.g. to compare with something, to send
+    # public key to node, and so on...), so missing key generator executable
+    # is not a problem.
+
+
+def test_if_serialization_fails_due_to_missing_key_generator_executable():  # pylint: disable=invalid-name
+    account = tt.Account('example')
+
+    for key in ['private_key', 'public_key']:
+        with pytest.raises(FileNotFoundError):
+            str(getattr(account, key))  # Run serialization, but it requires key generator, so should fail
