664d108633bc999a1dff64f733c78880f7557e2b
Piotr Batko <pbatko@syncad.com>
2022-08-22 13:36:28 +0200

    Pass secret by initializer instead of manual setting with setter

--- package/test_tools/__private/account.py
+++ package/test_tools/__private/account.py
@@ -18,8 +18,7 @@ class Account:
     def create_multiple(number_of_accounts, name_base='account', *, secret='secret'):
         accounts = []
         for generated in KeyGenerator.generate_keys(name_base, number_of_accounts=number_of_accounts, secret=secret):
-            account = Account(generated['account_name'], with_keys=False)
-            account.secret = secret
+            account = Account(generated['account_name'], secret=secret, with_keys=False)
             account.private_key = generated['private_key']
             account.public_key = generated['public_key']
 
