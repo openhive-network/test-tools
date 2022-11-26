76607f196340b2554ffd112f0545901034c8d2ca
Mateusz Żebrak <mzebrak@syncad.com>
2022-11-26 13:12:01 +0100

    Make DecimalConverter precision optional

--- package/test_tools/__private/utilities/decimal_converter.py
+++ package/test_tools/__private/utilities/decimal_converter.py
@@ -1,18 +1,22 @@
 from decimal import Decimal
-from typing import Union
+from typing import Optional, Union
 import warnings
 
 
 class DecimalConverter:
     @classmethod
-    def convert(cls, amount: Union[int, float, str], precision: int) -> Decimal:
-        cls.__warn_if_precision_might_be_lost(amount, precision)
+    def convert(cls, amount: Union[int, float, str], precision: Optional[int] = None) -> Decimal:
+        if precision is not None:
+            cls.__warn_if_precision_might_be_lost(amount, precision)
 
         # We could not pass float variable directly to Decimal initializer as from the nature of floats it won't result
         # in the exact decimal value. We need to convert float to string first like https://stackoverflow.com/a/18886013
         # For example: `str(Decimal(0.1)) == '0.1000000000000000055511151231257827021181583404541015625'` is True
         if isinstance(amount, float):
             amount = repr(amount)
+
+        if precision is None:
+            return Decimal(amount).normalize()
 
         exponent = Decimal(10) ** (-1 * precision)
         return Decimal(amount).quantize(exponent).normalize()
