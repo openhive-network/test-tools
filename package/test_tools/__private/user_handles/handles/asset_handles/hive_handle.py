aaaec442d357427809f9e008be740442fd47fbe1
Mateusz Żebrak <mzebrak@syncad.com>
2022-11-28 11:29:17 +0100

    Create all token handles

---
+++ package/test_tools/__private/user_handles/handles/asset_handles/hive_handle.py
@@ -0,0 +1,9 @@
+from typing import Union
+
+from test_tools.__private.asset.asset import Asset
+from test_tools.__private.user_handles.handles.asset_handles.token_handle import TokenHandleBase
+
+
+class HiveHandle(TokenHandleBase):
+    def __init__(self, amount: Union[int, float]) -> None:
+        super().__init__(amount, token=Asset.Hive)
