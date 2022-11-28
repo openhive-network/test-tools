6f2c34dac5899095957c367e0bd4809626554d4b
Mateusz Żebrak <mzebrak@syncad.com>
2022-11-28 12:08:27 +0100

    fixup tokenhandlebase

--- package/test_tools/__private/user_handles/handles/asset_handles/token_handle.py
+++ package/test_tools/__private/user_handles/handles/asset_handles/token_handle.py
@@ -5,7 +5,7 @@ import typing
 from typing import Any, Type, Union
 
 from test_tools.__private.asset.token import Token
-from test_tools.__private.user_handles.get_implementation import get_implementation
+from test_tools.__private.user_handles.get_implementation import get_handle, get_implementation
 from test_tools.__private.user_handles.inside_static_handle import InsideStaticHandle
 
 
@@ -31,17 +31,17 @@ class TokenHandleBase(InsideStaticHandle):
 
     def __neg__(self) -> TokenHandleBase:
         token = -self.__implementation
-        return self.__create_handle_from_implementation(token)
+        return get_handle(token)
 
     def __add__(self, other: Any) -> TokenHandleBase:
         other = self.__get_implementation_if_exists(other)
         token = self.__implementation + other
-        return self.__create_handle_from_implementation(token)
+        return get_handle(token)
 
     def __sub__(self, other: Any) -> TokenHandleBase:
         other = self.__get_implementation_if_exists(other)
         token = self.__implementation - other
-        return self.__create_handle_from_implementation(token)
+        return get_handle(token)
 
     def __iadd__(self, other: Any) -> TokenHandleBase:
         other = self.__get_implementation_if_exists(other)
@@ -66,10 +66,6 @@ class TokenHandleBase(InsideStaticHandle):
         if isinstance(other, TokenHandleBase):
             return get_implementation(other)
         return other
-
-    def __create_handle_from_implementation(self, implementation: Token) -> TokenHandleBase:
-        my_type = type(self)
-        return my_type(__implementation=implementation)
 
     def as_nai(self) -> dict:
         """Returns the token in the NAI representation (JSON format as a dictionary)."""
