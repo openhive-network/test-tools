ddd4c38b5476bec83cab39f691f96f91abff838d
Mateusz Żebrak <mzebrak@syncad.com>
2022-12-13 11:55:27 +0100

    Use node fixture in test_wait_for

--- tests/functional_tests/node_tests/test_wait_for_irreversible.py
+++ tests/functional_tests/node_tests/test_wait_for_irreversible.py
@@ -3,21 +3,22 @@ import pytest
 import test_tools as tt
 
 
-def test_raising_timeout():
-    # ARRANGE
+@pytest.fixture(name="node")
+def _node():
     node = tt.InitNode()
     node.run()
+    return node
 
+
+def test_raising_timeout(node: tt.InitNode):
     # ACT & ASSERT
     with pytest.raises(TimeoutError):
         node.wait_for_irreversible_block(100, timeout=0.1)
 
 
 @pytest.mark.parametrize("include_number_param", [False, True], ids=("current_head_block", "block_with_number"))
-def test_waiting_for_irreversible_block(include_number_param: bool):
+def test_waiting_for_irreversible_block(include_number_param: bool, node: tt.InitNode):
     # ARRANGE
-    node = tt.InitNode()
-    node.run()
     last_block_number = node.get_last_block_number()
 
     # ACT
