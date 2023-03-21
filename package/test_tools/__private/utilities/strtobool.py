20b70e4006083b7a20222428bcaff9b7474ba921
kmochocki <kmochocki@syncad.com>
2023-03-21 10:40:05 +0000

    Move strtobool from distutils as internal utility function of test_tools

--- package/test_tools/__private/utilities/strtobool.py
+++ package/test_tools/__private/utilities/strtobool.py
@@ -1,21 +1,24 @@
-_MAP = {
-    "y": True,
-    "yes": True,
-    "t": True,
-    "true": True,
-    "on": True,
-    "1": True,
-    "n": False,
-    "no": False,
-    "f": False,
-    "false": False,
-    "off": False,
-    "0": False,
-}
+"""
+This is copied function from distutils.util, because this
+module is deprecated and will be removed
+
+source: https://peps.python.org/pep-0632/
+copy idea: https://stackoverflow.com/a/71133268/11738218
+"""
 
 
-def strtobool(value) -> bool:
-    try:
-        return _MAP[str(value).lower()]
-    except KeyError as exc:
-        raise ValueError(f"{value} is not a valid bool value") from exc
+def strtobool(val: str) -> bool:
+    """Convert a string representation of truth to true (1) or false (0).
+
+    True values are 'y', 'yes', 't', 'true', 'on', and '1'; false values
+    are 'n', 'no', 'f', 'false', 'off', and '0'.  Raises ValueError if
+    'val' is anything else.
+    """
+    val = val.lower()
+    if val in ("y", "yes", "t", "true", "on", "1"):
+        return True
+
+    if val in ("n", "no", "f", "false", "off", "0"):
+        return False
+
+    raise ValueError(f"invalid truth value {val}")
