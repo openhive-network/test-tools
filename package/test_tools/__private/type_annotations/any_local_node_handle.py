f7f0e4ccb82d1213812863e17c9977af7152c6c8
Mateusz Kudela <mkudela@syncad.com>
2023-04-03 14:23:06 +0200

    Add FullApiNode to AnyNode handle

--- package/test_tools/__private/type_annotations/any_local_node_handle.py
+++ package/test_tools/__private/type_annotations/any_local_node_handle.py
@@ -4,8 +4,9 @@ if TYPE_CHECKING:
     from typing import Union
 
     from test_tools.__private.user_handles.handles.node_handles.api_node_handle import ApiNodeHandle
+    from test_tools.__private.user_handles.handles.node_handles.full_api_node_handle import FullApiNodeHandle
     from test_tools.__private.user_handles.handles.node_handles.init_node_handle import InitNodeHandle
     from test_tools.__private.user_handles.handles.node_handles.raw_node_handle import RawNodeHandle
     from test_tools.__private.user_handles.handles.node_handles.witness_node_handle import WitnessNodeHandle
 
-    AnyLocalNodeHandle = Union[ApiNodeHandle, InitNodeHandle, RawNodeHandle, WitnessNodeHandle]
+    AnyLocalNodeHandle = Union[ApiNodeHandle, FullApiNodeHandle, InitNodeHandle, RawNodeHandle, WitnessNodeHandle]
