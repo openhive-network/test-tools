cbeabaa5c8fcb54289d9aa676088563777da8ff5
Michał Kudela <kudmich@syncad.com>
2023-08-07 10:36:49 +0200

    Expand the utility of 'init_node' object by adding 'set_vest_price' function

--- package/test_tools/__private/init_node.py
+++ package/test_tools/__private/init_node.py
@@ -2,6 +2,10 @@ from __future__ import annotations
 
 from typing import Optional, TYPE_CHECKING
 
+from test_tools.__private.asset import Asset
+from test_tools.__private.exceptions import NodeIsNotRunning
+from test_tools.__private.logger.logger_internal_interface import logger
+from test_tools.__private.vest_price import VestPrice
 from test_tools.__private.witness_node import WitnessNode
 
 if TYPE_CHECKING:
@@ -16,3 +20,33 @@ class InitNode(WitnessNode):
         super().__init__(name=name, network=network, witnesses=["initminer"], handle=handle)
         self.config.enable_stale_production = True
         self.config.required_participation = 0
+
+    def set_vest_price(self, base: Asset.Test, quote: Asset.Vest, invest: Asset.Test = Asset.Test(10_000_000)) -> None:
+        if not self.is_running():
+            raise NodeIsNotRunning("Cannot set a price on a disabled node. Please start node before setting the price.")
+
+        price = VestPrice(base=base, quote=quote)
+        self.wait_for_block_with_number(2)
+        self.__set_new_price(price)
+        if invest > Asset.Test(0):
+            self.__stabilize_the_price(invest=invest)
+        self.__log_vest_price_from_network()
+
+    def __set_new_price(self, new_price) -> None:
+        # FIXME: https://gitlab.syncad.com/hive/hive/-/issues/254#note_131758  # pylint: disable=fixme
+        logger.info(f"new vests price (wrapped) {new_price}.")
+        self.api.debug_node.debug_set_vest_price(vest_price=new_price.as_nai())
+
+    def __stabilize_the_price(self, invest: Asset.Test) -> None:
+        from test_tools.__private.wallet import Wallet  # pylint: disable=import-outside-toplevel, cyclic-import
+
+        wallet = Wallet(attach_to=self)
+
+        # this is required to minimize inflation impact on vest price
+        wallet.api.transfer_to_vesting("initminer", "initminer", invest)
+        logger.info(f"Price stabilization completed. Invested: {invest} in network.")
+        wallet.close()
+
+    def __log_vest_price_from_network(self) -> None:
+        dgpo = self.api.wallet_bridge.get_dynamic_global_properties()
+        logger.info(f"new vests price (real): {VestPrice.from_dgpo(dgpo)}")
--- package/test_tools/__private/user_handles/handles/node_handles/init_node_handle.py
+++ package/test_tools/__private/user_handles/handles/node_handles/init_node_handle.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
 
-from typing import Optional, TYPE_CHECKING
+from typing import cast, Optional, TYPE_CHECKING
 
 from test_tools.__private.init_node import InitNode
 from test_tools.__private.user_handles.get_implementation import get_implementation
 from test_tools.__private.user_handles.handles.node_handles.runnable_node_handle import RunnableNodeHandle
+from test_tools.__private.vest_price import Asset
 
 if TYPE_CHECKING:
     from test_tools.__private.user_handles.handles.network_handle import NetworkHandle as Network
@@ -18,3 +19,20 @@ class InitNodeHandle(RunnableNodeHandle):
                 handle=self,
             )
         )
+
+    @property
+    def __implementation(self) -> InitNode:
+        return cast(InitNode, get_implementation(self))
+
+    def set_vest_price(
+        self, quote: Asset.Vest, base: Asset.Test = Asset.Test(1), invest: Asset.Test = Asset.Test(10_000_000)
+    ) -> object:
+        """
+        Used to set a new price for vests in relation to hive on a blockchain node. It calculates the new price,
+        logs it, and then updates the vest price on the node using the API call.
+        :param invest: The hive amount to be invested in the network to mitigate inflation during the price change.
+        :param base: The base asset for the price calculation (hive).
+        :param quote: The quote asset for the price calculation (vests).
+        :raises RuntimeError: When the node is not running.
+        """
+        return self.__implementation.set_vest_price(base, quote, invest)
