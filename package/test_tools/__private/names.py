1d6120dfe96c603c56dc196eeed189584c2a293e
kmochocki <kmochocki@syncad.com>
2023-10-10 19:40:09 +0000

    Add annotations to names module

--- package/test_tools/__private/names.py
+++ package/test_tools/__private/names.py
@@ -1,18 +1,16 @@
 from __future__ import annotations
 
-from typing import Optional, Set
-
-from test_tools.__private.exceptions import NameAlreadyInUse
+from test_tools.__private.exceptions import NameAlreadyInUseError
 
 
 class Names:
-    def __init__(self, parent: Optional[Names] = None):
-        self.__unique_names = set()
-        self.__next_name_numbers = {}
+    def __init__(self, parent: Names | None = None) -> None:
+        self.__unique_names: set[str] = set()
+        self.__next_name_numbers: dict[str, int] = {}
 
         self.__parent = parent
 
-    def get_names_in_use(self) -> Set[str]:
+    def get_names_in_use(self) -> set[str]:
         numbered_names = set()
         for name, number_limit in self.__next_name_numbers.items():
             numbered_names.update([f"{name}{i}" for i in range(number_limit)])
@@ -20,7 +18,6 @@ class Names:
         return self.__unique_names | numbered_names
 
     def __get_numbered_name_next_free_number(self, name: str) -> int:
-        # pylint: disable=protected-access
         # Accessing another instance private member of the same class is not a privacy violation.
         parent_next_free_number = self.__parent.__get_numbered_name_next_free_number(name) if self.__parent else 0
         my_next_free_number = self.__next_name_numbers.get(name, 0)
@@ -35,8 +32,8 @@ class Names:
 
         return name_with_number
 
-    def register_unique_name(self, name: str):
+    def register_unique_name(self, name: str) -> None:
         if name in self.__unique_names:
-            raise NameAlreadyInUse(f'Name "{name}" is already in use')
+            raise NameAlreadyInUseError(f'Name "{name}" is already in use')
 
         self.__unique_names.add(name)
