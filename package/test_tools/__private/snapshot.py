196beaf536c6f4e6a2e6ff5d39982fa112908781
kmochocki <kmochocki@syncad.com>
2023-10-10 19:38:04 +0000

    Add annotations to snapshot module

--- package/test_tools/__private/snapshot.py
+++ package/test_tools/__private/snapshot.py
@@ -1,16 +1,24 @@
+from __future__ import annotations
+
+import contextlib
 import filecmp
-from glob import glob as get_files_matching_pattern
 import json
-from pathlib import Path
 import shutil
-from typing import Optional
+from typing import TYPE_CHECKING, Any
+
+if TYPE_CHECKING:
+    from pathlib import Path
+
+    from test_tools.__private.node import Node
 
 
 class Snapshot:
-    def __init__(self, snapshot_path: Path, block_log_path: Path, block_log_artifacts_path: Path, node=None):
+    def __init__(
+        self, snapshot_path: Path, block_log_path: Path, block_log_artifacts_path: Path, node: Node | None = None
+    ) -> None:
         self.__snapshot_path: Path = snapshot_path
         self.__block_log_path: Path = block_log_path
-        self.__block_log_artifacts_path: Optional[Path] = (
+        self.__block_log_artifacts_path: Path | None = (
             block_log_artifacts_path if block_log_artifacts_path.exists() else None
         )
         self.__creator = node
@@ -21,10 +29,10 @@ class Snapshot:
                 self.state = None
                 return
 
-            with open(snapshot_state_path, encoding="utf-8") as state_file:
+            with snapshot_state_path.open(encoding="utf-8") as state_file:
                 self.state = json.load(state_file)
 
-    def copy_to(self, node_directory: Path):
+    def copy_to(self, node_directory: Path) -> None:
         block_log_directory = node_directory / "blockchain"
         block_log_directory.mkdir(exist_ok=True)
 
@@ -36,31 +44,32 @@ class Snapshot:
             shutil.copytree(self.__snapshot_path, destination_snapshot_path)
 
     @staticmethod
-    def __copy_file(source: Optional[Path], destination: Path, *, allow_missing: bool = False):
-        if allow_missing and source is None:
+    def __copy_file(source: Path | None, destination: Path, *, allow_missing: bool = False) -> None:
+        if allow_missing and source is None or source is None:
             return
 
-        try:
+        with contextlib.suppress(
+            shutil.SameFileError
+        ):  # It's ok, just skip copying because user want to load node's own snapshot.
             shutil.copy(source, destination)
-        except shutil.SameFileError:
-            # It's ok, just skip copying because user want to load node's own snapshot.
-            pass
 
     def get_path(self) -> Path:
         return self.__snapshot_path
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         optional_creator_info = "" if self.__creator is None else f" from {self.__creator}"
         return f"<Snapshot{optional_creator_info}: path={self.__snapshot_path}>"
 
-    def __eq__(self, other) -> bool:
-        my_files = sorted(get_files_matching_pattern(f"{self.get_path()}/**/*.sst", recursive=True))
-        others_files = sorted(get_files_matching_pattern(f"{other.get_path()}/**/*.sst", recursive=True))
+    def __eq__(self, other: Any) -> bool:
+        assert isinstance(other, Snapshot)
+
+        my_files = sorted(self.get_path().rglob("*.sst"))
+        others_files = sorted(other.get_path().rglob("*.sst"))
 
         if len(my_files) != len(others_files):
             return False
 
-        for mine, others in zip(my_files, others_files):
+        for mine, others in zip(my_files, others_files, strict=False):
             files_are_same = filecmp.cmp(mine, others, shallow=False)
             if not files_are_same:
                 return False
