353d88fec6c0cdd7e63d3cdb302d704cd3562993
kmochocki <kmochocki@syncad.com>
2023-10-10 19:25:16 +0000

    Fix type annoteted package

--- package/test_tools/__private/type_annotations/any_node_handle.py
+++ package/test_tools/__private/type_annotations/any_node_handle.py
@@ -1,9 +1,6 @@
-from typing import TYPE_CHECKING
+from __future__ import annotations
 
-if TYPE_CHECKING:
-    from typing import Union
+from test_tools.__private.type_annotations.any_local_node_handle import AnyLocalNodeHandle
+from test_tools.__private.user_handles.handles.node_handles.remote_node_handle import RemoteNodeHandle
 
-    from test_tools.__private.type_annotations.any_local_node_handle import AnyLocalNodeHandle
-    from test_tools.__private.user_handles.handles.node_handles.remote_node_handle import RemoteNodeHandle
-
-    AnyNodeHandle = Union[AnyLocalNodeHandle, RemoteNodeHandle]
+AnyNodeHandle = AnyLocalNodeHandle | RemoteNodeHandle
