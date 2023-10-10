47c9872a7f1fbae6b2d455bc80ea3a99b4b2f8ba
kmochocki <kmochocki@syncad.com>
2023-10-10 19:34:08 +0000

    Refactor VestPrice to use schemas

--- package/test_tools/__private/vest_price.py
+++ package/test_tools/__private/vest_price.py
@@ -1,27 +1,33 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
+from typing import TYPE_CHECKING
 
-from test_tools.__private.asset import Asset
+from schemas.fields.compound import Price
+
+if TYPE_CHECKING:
+    from helpy import Hf26Asset
+    from schemas.apis.database_api import GetDynamicGlobalProperties
 
 
 @dataclass
 class VestPrice:
-    base: Asset.Vest
-    quote: Asset.Test
+    base: Hf26Asset.Vests | Hf26Asset.Test
+    quote: Hf26Asset.Test | Hf26Asset.Vests
 
     def __str__(self) -> str:
         ratio = self.quote.amount / self.base.amount / 10**self.base.precision
-        return f"{ratio} {self.quote.token} per 1 {self.base.token}"
+        return f"{ratio} {self.quote.get_asset_information().get_symbol()} per 1 {self.base.get_asset_information().get_symbol()}"
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}({self.as_nai()})"
 
     @classmethod
-    def from_dgpo(cls, dgpo: dict) -> VestPrice:
-        vest: Asset.Vest = Asset.from_(dgpo["total_vesting_shares"])
-        hive: Asset.Test = Asset.from_(dgpo["total_vesting_fund_hive"])
-        return cls(quote=vest, base=hive)
+    def from_dgpo(cls, dgpo: GetDynamicGlobalProperties) -> VestPrice:
+        return cls(quote=dgpo.total_vesting_shares, base=dgpo.total_vesting_fund_hive)
 
-    def as_nai(self) -> dict:
-        return {"quote": self.quote.as_nai(), "base": self.base.as_nai()}
+    def as_nai(self) -> dict[str, dict[str, str]]:
+        return {"quote": self.quote.dict(), "base": self.base.dict()}
+
+    def as_schema(self) -> Price[Hf26Asset.Test, Hf26Asset.Tbd, Hf26Asset.Vests]:
+        return Price(base=self.base, quote=self.quote)
