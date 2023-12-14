8c4b260aa1082a975f9b14a9c63dc4e822faa520
asuch <asuch@syncad.com>
2023-12-14 12:53:51 +0100

    Update enabled plugin list in preconfigured node

--- package/test_tools/__private/preconfigured_node.py
+++ package/test_tools/__private/preconfigured_node.py
@@ -16,7 +16,17 @@ class PreconfiguredNode(RawNode):
         super().__init__(name=name, network=network, handle=handle)
 
         self._enable_api_plugins(
-            plugins=["block_api", "database_api", "debug_node_api", "network_node_api", "wallet_bridge_api"]
+            plugins=[
+                "block_api",
+                "database_api",
+                "debug_node_api",
+                "network_node_api",
+                "wallet_bridge_api",
+                "account_by_key_api",
+                "market_history_api",
+                "network_broadcast_api",
+                "rc_api",
+            ]
         )
         self.config.log_logger = (
             '{"name":"default","level":"debug","appender":"stderr"} '
