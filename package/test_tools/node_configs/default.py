ce06f5d9348bb1782842461a1b2b33e14572599d
kmochocki <kmochocki@syncad.com>
2024-09-27 14:25:49 +0000

    Set log level for hived in default config

--- package/test_tools/node_configs/default.py
+++ package/test_tools/node_configs/default.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import json
+
 from test_tools.__private.node_config import NodeConfig
 
 
@@ -11,10 +13,21 @@ def create_default_config(*, skip_address: bool = False) -> NodeConfig:
     -------
         NodeConfig: with default values.
     """
+    hived_logger_settings = [
+        {"name": "default", "level": "debug", "appenders": ["stderr"]},
+        {"name": "user", "level": "debug", "appenders": ["stderr"]},
+        {"name": "p2p", "level": "info", "appenders": ["p2p"]},
+    ]
+
     return NodeConfig(
         **(
-            {"p2p_endpoint": "0.0.0.0:0", "webserver_http_endpoint": "0.0.0.0:0", "webserver_ws_endpoint": "0.0.0.0:0"}
+            {
+                "p2p_endpoint": "0.0.0.0:0",
+                "webserver_http_endpoint": "0.0.0.0:0",
+                "webserver_ws_endpoint": "0.0.0.0:0",
+            }
             if not skip_address
             else {}
         ),
+        log_logger=" ".join(json.dumps(item) for item in hived_logger_settings),
     )
