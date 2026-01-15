26dfedb81514646c80d5f67885510ac0e37e942d
Mateusz Kudela <mkudela@syncad.com>
2026-01-15 14:16:25 +0000

    Remove tests moved to hive repository
    - Remove functional_tests/ (all tests require hived)
    - Remove unit tests with requires_hived_executables marker:
      - key_generation_tests/test_key_comparison.py
      - key_generation_tests/test_key_generation.py
      - communication_tests/test_keys_serialization.py
      - network_tests/test_networks_connecting.py
    
    These tests are now in hive/tests/python/test_tools_tests/
    and run with locally built hived in hive CI.

--- tests/unit_tests/network_tests/conftest.py
+++
@@ -1,7 +0,0 @@
-# pylint: disable=unused-import
-# These fixtures are used by pytest, so it's false positive.
-from __future__ import annotations
-
-from local_tools.network import four_networks_connected, three_networks_connected, two_networks_connected  # noqa: F401
-
-# pylint: enable=unused-import
